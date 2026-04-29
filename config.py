a0e7b092c1b942a65b66ff22697752b536376b19
ashu302k <llife8223@gmail.com>
2026-04-29 14:11:01 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+BOT_TOKEN = "8624458357:AAGEpLyYj1pw25XQRZsZgnuGx-gchy11ktI"
+OWNER_ID = 5182241674
+
+DATABASE_URL = "sqlite:///mlbb_bot.db"
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
